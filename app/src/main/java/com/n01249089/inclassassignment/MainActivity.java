9e4469ffccd7c838824d38c9bc2510f40068ae11
Manshur7 <54862207+Manshur7@users.noreply.github.com>
2019-09-18 13:26:33 -0400

    Initial commit

---
+++ app/src/main/java/com/n01249089/inclassassignment/MainActivity.java
@@ -0,0 +1,28 @@
+package com.n01249089.inclassassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button but;
+        but = findViewById(R.id.button);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hello, this is my First Android App",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
